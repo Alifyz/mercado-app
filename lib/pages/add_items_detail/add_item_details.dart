38cdb8ecca1ef24fd2e3288c8f7f1001a36089ac
Alifyz Pires <alifyzdev@gmail.com>
2020-03-21 15:10:59 -0300

    refactor: grocery list items widget

--- lib/pages/add_items_detail/add_item_details.dart
+++ lib/pages/add_items_detail/add_item_details.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:groceryapp/domain/grocery_item.dart';
 import 'package:groceryapp/pages/add_items_detail/utils/arguments.dart';
+import 'package:groceryapp/pages/add_items_detail/utils/details_utils.dart';
+import 'package:groceryapp/pages/add_items_detail/widgets/items_details_widget.dart';
 import 'package:groceryapp/repository/repository.dart';
 
 class AddItemsDetailsPage extends StatelessWidget {
@@ -15,61 +17,23 @@ class AddItemsDetailsPage extends StatelessWidget {
       body: FutureBuilder(
           future: Repository.getAllGroceries(),
           builder: (context, AsyncSnapshot<List<GroceryItem>> snapshot) {
-            switch (snapshot.connectionState) {
-              case ConnectionState.done:
-                {
-                  if (snapshot.hasData) {
-                    return ListView.builder(
-                      itemCount: snapshot.data.length,
-                      itemBuilder: (context, index) {
-                        return ListTile(
-                          title: Text(snapshot.data[index].title),
-                        );
-                      },
-                    );
-                  }
-                  if (snapshot.hasError) {
-                    return Center(
-                      child: Text('has error'),
-                    );
-                  }
-                }
-                break;
-              case ConnectionState.waiting:
-                {
-                  return Center(
-                    child: CircularProgressIndicator(),
-                  );
-                }
-              default:
-                {
-                  return Center(
-                    child: CircularProgressIndicator(),
-                  );
-                }
+            if (!snapshot.hasError && snapshot.hasData) {
+              return GroceryListWidget(data: snapshot.data);
+            }
+
+            if (snapshot.connectionState != ConnectionState.done) {
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+
+            if (snapshot.hasError) {
+              return Center(
+                child: Text('Erro ao carregar items da lista'),
+              );
             }
           }),
     );
   }
 }
 
-String getHeader(Category category) {
-  switch (category) {
-    case Category.Meat:
-      return "carnes";
-      break;
-    case Category.Milk:
-      return "laticínios";
-      break;
-    case Category.Cleaning:
-      return "limpeza";
-      break;
-    case Category.Fiber:
-      return "legumes";
-      break;
-    default:
-      {
-        return "items";
-      }
-  }
-}
