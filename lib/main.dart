b40984899d97bb0cad3376e551f594ef3eaae213
Alifyz <alifyzdev@gmail.com>
2020-04-21 18:24:06 -0300

    add: page transitions

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,8 @@ import 'package:groceryapp/pages/user_list_details/user_list_details.dart';
 import 'package:groceryapp/repository/repository.dart';
 import 'package:provider/provider.dart';
 
+import 'pages/add_items/user_list_detail.dart';
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
@@ -18,6 +20,20 @@ class MyApp extends StatelessWidget {
       child: Provider(
         create: (_) => new Repository(),
         child: MaterialApp(
+          onGenerateRoute: (settings) {
+            if (settings.name == UserListPage.route) {
+              return PageRouteBuilder(
+                  pageBuilder: (_, __, ___) => UserListPage(),
+                  transitionsBuilder: (context, animStart, animEnd, child) {
+                    final begin = Offset(0, 0);
+                    final end = Offset.zero;
+                    final tween = Tween(begin: begin, end: end);
+                    final offsetAnimation = animStart.drive(tween);
+                    return SlideTransition(
+                        position: offsetAnimation, child: child);
+                  });
+            }
+          },
           routes: {
             HomePage.route: (context) => HomePage(),
             UserListPage.route: (context) => UserListPage(),
